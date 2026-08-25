705d9ee54555f24fcbfe7de4c115ae61fd4cb47d
hotteacup-rgb <hotteacup11@gmail.com>
2026-08-24 23:43:40 -0700

    Track recent retirement date changes

--- removals.js
+++ removals.js
@@ -1,1 +1,4 @@
-window.DATA_REMOVALS=['40955']; // Maersk Dual-Fuel Container Vessel moved from Dec 2026 to Jul 2027 in the current retirement feed.
+window.DATA_REMOVALS=['40955'];
+window.RETIREMENT_CHANGES=[
+  {setno:'40955',name:'Maersk Dual-Fuel Container Vessel',from:'December 2026',to:'July 2027',note:'Removed from the 2026 tracker after the current retirement feed moved it into 2027.'}
+];
